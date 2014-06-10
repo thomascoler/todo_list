2f5c91d7971f1eb89006966704e7b8774b08ec3e
Thomas Coler <thomas@valueoutlook.com>
2014-06-10 07:42:59 -0600

    right before major problem with Edit link

---
+++ app/models/todo_list.rb
@@ -0,0 +1,9 @@
+class TodoList < ActiveRecord::Base
+	has_many :todo_items
+	
+	
+	validates :title, presence: true
+	validates :title, length: { minimum: 3 }
+	validates :description, presence: true
+	validates :description, length: { minimum: 5 }
+end
---
+++ spec/models/todo_list_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe TodoList, :type => :model do
+  it { should have_many(:todo_items) }
+
+end
